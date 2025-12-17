df14e6b87c953536b2b50c3a3c5b3a6de4d89fa5
Ev777gen <fortheartist@mail.ru>
2025-12-17 08:40:10 +0500

    add example: fetch random cocktail

---
+++ examples/ex-06-fetch-a-cocktail (using framework v_3.0.0)/index.js
@@ -0,0 +1,71 @@
+import { h, hFragment, defineComponent } from "../../packages/runtime/dist/krowmare.js"
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+
+async function getRandomCocktail() {
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => json.drinks.at(0))
+}
+
+const RandomCocktail = defineComponent({
+  state() {
+    return { 
+      cocktail: null,
+      isLoading: false,
+    }
+  },
+
+  render() {
+    const { cocktail, isLoading } = this.state
+
+    if (isLoading) {
+      return hFragment([
+        h('h1', {}, ['Random cocktail']),
+        h('p', {}, ['Loading...'])
+      ])
+    }
+
+    if (!cocktail) {
+      return hFragment([
+        h('h1', {}, ['Random cocktail']),
+        h(
+          'button',
+          {
+            on: {
+              click: () => this.fetchCocktail(),
+            },
+          },
+          ['Get a cocktail'],
+        )
+      ])
+    }
+    else {
+      return hFragment([
+        h('h1', {}, [cocktail.strDrink]),
+        h('p', {}, [cocktail.strInstructions]),
+        h('img', { src: cocktail.strDrinkThumb, alt: cocktail.strDrink }),
+        h(
+          'button',
+          {
+            on: {
+              click: () => this.fetchCocktail(),
+            },
+          },
+          ['Get another cocktail'],
+        ),
+      ])
+    }
+  },
+
+  async fetchCocktail() {
+    this.updateState({ isLoading: true, cocktail: null })
+
+    const cocktail = await getRandomCocktail()
+    this.updateState({ isLoading: false, cocktail })
+  }
+})
+
+const cocktail = new RandomCocktail()
+
+cocktail.mount(document.body)
