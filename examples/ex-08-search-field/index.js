fd41447023a5b7b018bf0a34edc63bb1bcfc91de
Ev777gen <fortheartist@mail.ru>
2025-12-19 08:00:30 +0500

    add search field example

---
+++ examples/ex-08-search-field/index.js
@@ -0,0 +1,38 @@
+import { h, hFragment, defineComponent } from "../../packages/runtime/dist/krowmare.js"
+
+const SearchField = defineComponent({
+  state() {
+    return {}
+  },
+  
+  render() {
+    return hFragment([
+      h('h1', {}, ['Search Field']),
+      h(
+        InputField,
+        {
+          on: {
+            'input-data': (data) => console.log(data),
+          }
+        }
+      ),
+    ])
+  },
+})
+
+const InputField = defineComponent({
+  render() {
+    return h(
+      'input',
+      { 
+        on: {
+          input: (data) => this.emit('input-data', data),
+        },
+      },
+    )
+  },
+})
+
+const searchField = new SearchField()
+
+searchField.mount(document.body)
